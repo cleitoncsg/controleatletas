24c62dfba4b810a368ea47e3a3f182ed3f033fb3
cleitoncsg <cleiton.tom@hotmail.com>
2013-06-01 23:45:57 -0300

    Implementando mais testes

--- ControleAtleta/src/br/unb_fga/enxadrista/testaControleAtleta/TestarControleEnxadrista.java
+++ ControleAtleta/src/br/unb_fga/enxadrista/testaControleAtleta/TestarControleEnxadrista.java
@@ -1,42 +1,13 @@
-package br.unb.sece.junit;
+package br.unb_fga.enxadrista.testaControleAtleta;
+import br.unb_fga.enxadrista.model.Enxadrista;
+import static org.junit.Assert.assertNotNull;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
-import org.junit.Before;
-import org.junit.Test;
-
-
-
-public class TestarControleNota {
+public class TestarControleEnxadrista{
 
 	private Enxadrista enxadrista;
-	
 
 	@Test
 	public void testarInstancia(){
 		assertNotNull(Enxadrista);
-	}
-
-	
-	@Test
-	public void testarSelectTurmaCerto()
-	{
-		try
-		{
-			CNota.getModelTurma().setSelectedItem("1 - turma");
-			CNota.selectTurma("1 - turma");
-		}
-		catch(Exception e)
-		{
-			fail("Ocorreu um erro");
-			//e.printStackTrace();
-		}
-	}
-	
-
-	
-
-
+	}	
 }
